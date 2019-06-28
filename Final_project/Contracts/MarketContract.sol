4329345abb479d0b454adeb245bd03c013a986b7
lghfoo <45117364+lghfoo@users.noreply.github.com>
2019-06-28 14:03:25 +0800

    update market contract

--- Final_project/Contracts/MarketContract.sol
+++ Final_project/Contracts/MarketContract.sol
@@ -1,69 +1,103 @@
 pragma solidity ^0.4.25;
 contract CardManagementInterface{
-    //get Card id
-    function getPriceOf(uint cardId)external returns(uint256);
+    function getPriceOf(uint cardId)external returns(uint);
     function getCardOwner(uint cardId)external returns(address);
     function setCardOwner(uint cardId, address owner)external;
     function setCardOnSale(uint cardId, bool onSale)external;
+    function setCardPrice(uint cardId, uint price)external;
+    function createCard(uint cardId, string url, int8 level);
 }
 
 contract AccountManagementInterface{
-    function getBalanceOf(address addr)external returns(uint256);
+    function getBalanceOf(address addr)external returns(uint);
+    function setBalanceOf(address addr, uint balance)external;
     function removeCard(address owner, uint cardId)external;
     function addCard(address who, uint cardId)external;
-    //get balance of 
-    //set balance of 
+    function getDrawCountOf(address addr)external returns(uint32);
+    function setDrawCountOf(address addr, uint count)external;
 }
 
 contract MarketContract{
     uint[] cardsOnSale;
     AccountManagementInterface accountManagementInterface;
     CardManagementInterface cardManagementInterface;
-    function recharge(address addr, uint256 ammount){
-        // accountManagementContract.recharge(addr, ammount);
+    
+    function setAccoundManagementInterface(address addr){
+        accountManagementInterface = AccountManagementInterface(addr);
     }
     
-    function setAccountManagementContract(address addr){
-        // accountManagementContract = AccountManagementContract(addr);
+    function setCardManagementInterface(address addr){
+        cardManagementInterface = CardManagementInterface(addr);
     }
     
-    function buyDrawCards(address add, int32 times){
-        //get balance
-        //...
-        //set balance
-        // accountManagementContract.buyDrawCards(add, times);
+    function recharge(address addr, uint amount){
+        uint balance = accountManagementInterface.getBalanceOf(addr);
+        accountManagementInterface.setBalanceOf(addr, balance + amount);
     }
     
-    function pushCard(address who, uint cardId, uint price){
-        
-        //get cardId
-        //cardsOnSale++
+    //0:success
+    //1:balance not enough
+    function buyDrawCards(address addr, uint times)returns(uint){
+        uint amountPerTime = 10;
+        uint balance = accountManagementInterface.getBalanceOf(addr);
+        uint needBalance = times*amountPerTime;
+        if(needBalance > balance){
+            return 1;
+        }
+        accountManagementInterface.setBalanceOf(addr, balance - needBalance);
+        uint32 count = accountManagementInterface.getDrawCountOf(addr);
+        accountManagementInterface.setDrawCountOf(addr, count + times);
+        return 0;
     }
     
-    function pullCard(uint cardId){
-        //cardsOnSale--
+    function pushCard(address who, uint cardId, uint price)returns(uint){
+        cardsOnSale.push(cardId);
+        cardManagementInterface.setCardOnSale(cardId, true);
+        cardManagementInterface.setCardPrice(cardId, price);
+        return 0;
     }
     
-    function buyCard(address who, uint cardId){
-        uint256 balance = accountManagementInterface.getBalanceOf(who);
-        uint256 price = cardManagementInterface.getPriceOf(cardId);
-        address oldOwner = cardManagementInterface.getCardOwner(cardId);
-        cardManagementInterface.setCardOwner(who);
-        accountManagementInterface.removeCard(oldOwner, cardId);
-        accountManagementInterface.addCard(who, cardId);
-        _removeCardsOnSale(cardId);
-        
-        //get cardId
-        //set card owner
-        //set owner card
-        //cardsOnSale--
+    //0:success
+    //1:card is not on sale
+    function pullCard(uint cardId)returns(uint){
+        cardManagementInterface.setCardOnSale(cardId, false);
+        return _removeCardOnSale(cardId);
+    }
+    
+    function buyCard(address buyer, uint cardId){
+        uint balanceOfBuyer = accountManagementInterface.getBalanceOf(buyer);
+        address seller = cardManagementInterface.getCardOwner(cardId);
+        uint balanceOfSeller = accountManagementInterface.getBalanceOf(seller);
+        uint price = cardManagementInterface.getPriceOf(cardId);
+        cardManagementInterface.setCardOwner(cardId, buyer);
+        cardManagementInterface.setCardOnSale(cardId, false);
+        accountManagementInterface.removeCard(seller, cardId);
+        accountManagementInterface.addCard(buyer, cardId);
+        accountManagementInterface.setBalanceOf(buyer, balanceOfBuyer -  price);
+        accountManagementInterface.setBalanceOf(seller, balanceOfSeller + price);
+        _removeCardOnSale(cardId);
+    }
+    
+    //0:success
+    //1:card is not on sale
+    function _removeCardOnSale(uint cardId)private returns(uint){
+        for(uint i = 0; i < cardsOnSale.length; i++){
+            if(cardsOnSale[i] == cardId){
+                for(uint j = i; j < cardsOnSale.length-1; j++){
+                    cardsOnSale[j] = cardsOnSale[j+1];
+                }
+                cardsOnSale.length--;
+                return 0;
+            }
+        }
+        return 1;
     }
     
     uint nonce = 0;
     function drawCard(string wish)external returns(int8, uint){
         uint cardId = uint(keccak256(abi.encodePacked(wish))) + 
                 uint(keccak256(now, msg.sender, nonce));
-        uint rand = cashId % 10000;
+        uint rand = cardId % 10000;
         nonce++;
         int8 level = 0;
         if(rand < 5000){
@@ -76,43 +110,14 @@ contract MarketContract{
             level = 4;
         }else if(rand <9890){
             level = 5;
-        }else(rand < 10000){
+        }else{
             level = 6;
         }
         return (level, cardId);
     }
     
-    function giveCardTo(address who, uint cardId, string url, int8 level){
-        
+    function createCardAndGiveTo(address who, uint cardId, string url, int8 level){
+        cardManagementInterface.createCard(cardId, url, level);
+        accountManagementInterface.addCard(who, cardId);
     }
-    
-    function _removeCardsOnSale(uint cardId)private{
-        //todo
-        for(;;){
-            
-        }
-        cardManagementInterface.setCardOnSale(cardId, false);
-    }
-    
-    //function changeCardOwner(string hashId, uint256 price, address _from, address _to) external returns(bool){
-    //     int index = -1;
-    //     for (uint i = 0; i<accounts[_from].cardsId.length; i++){
-    //         if(keccak256(accounts[_from].cardsId[i]) == keccak256(hashId) && price <= accounts[_to].balance){
-    //             index = int(i);
-    //             accounts[_to].balance = accounts[_to].balance - price;
-    //             accounts[_from].balance = accounts[_from].balance + price;
-    //             break;
-    //         }
-    //     }
-        
-    //     if(index != -1){
-    //         for(i = uint(index); i < accounts[_from].cardsId.length - 1; i++){
-    //             accounts[_from].cardsId[i] = accounts[_from].cardsId[i + 1];
-    //         }
-    //         accounts[_from].cardsId.length--;
-    //         accounts[_to].cardsId.push(hashId);
-    //         return true;
-    //     }
-    //     return false;
-    // }
 }
