6984e9dea6415f3e00db04cf648ee4c67c4ba5d5
AmoteChen <948167893@qq.com>
2019-06-28 14:24:42 +0800

    Cards合约

--- Final_project/Contracts/CardManagementContract.sol
+++ Final_project/Contracts/CardManagementContract.sol
@@ -15,4 +15,30 @@ contract CardManagementContract{
         require(cards[cardId].owner == msg.sender);
         cards[cardId].price = price;
     }
+    
+    function getPriceOf(uint cardId)external returns(uint){
+        require(cards[cardId].owner == msg.sender);
+        uint price = cards[cardId].price;
+        return price;
+    }
+    
+    function getCardOwner(uint cardId)external returns(address){
+        address ownerAddr = cards[cardId].owner;
+        return ownerAddr;
+    }
+    
+    function setCardOwner(uint cardId, address owner)external{
+        cards[cardId].owner = owner;
+    }
+    
+    function setCardOnSale(uint cardId, bool onSale)external{
+        cards[cardId].isOnSale = onSale;
+    }
+    
+    function createCard(uint cardId, string url, int8 level, address owner){
+        Card card = Card(level, cardId, url, false, 99999, owner);
+        cards[cardId] = card;
+        
+    }
+
 }
